76bcd3d35157059e85e04673087ea611477232cd
Rhana <159554135+RHAN4@users.noreply.github.com>
2025-05-27 11:24:35 -0300

    Add files via upload

---
+++ src/pages/BemVindo/index.js
@@ -0,0 +1,13 @@
+// src\pages\BemVindo\index.js
+import PaginaInicial from '../../components/PaginaInicial';
+import './styles.css';
+
+function Inicio() {
+    return (
+        <div className='pagina-inicial'>
+            <PaginaInicial />
+        </div>
+    );
+}
+
+export default Inicio;
